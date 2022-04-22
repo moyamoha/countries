012da3552c6f71d4280afb625835a2d179bcbb87
unknown <salimiyahya549@gmail.com>
2022-04-22 17:05:39 +0300

    add lint and workflow

--- src/pages/CountryDetail.tsx
+++ src/pages/CountryDetail.tsx
@@ -1,23 +1,21 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { useParams } from "react-router";
-import CountryCard from "../components/countryCard";
-import Paper from "@mui/material/Paper";
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router'
+import CountryCard from '../components/countryCard'
+import Paper from '@mui/material/Paper'
 
-import { InitialState } from "../redux/types";
-import { Country } from "../redux/types";
+import { InitialState } from '../redux/types'
+import { Country } from '../redux/types'
 
-import "./styles.scss";
+import './styles.scss'
 
 export default function CountryDetail() {
-	const countries = useSelector((state: InitialState) => state.countries);
-	const { country_name } = useParams();
-	const country = countries.data.find(
-		(c: Country) => c.name === country_name
-	) as Country;
-	return (
-		<Paper className="country-page" elevation={0}>
-			<CountryCard country={country}></CountryCard>
-		</Paper>
-	);
+  const countries = useSelector((state: InitialState) => state.countries)
+  const { country_name } = useParams()
+  const country = countries.data.find((c: Country) => c.name === country_name) as Country
+  return (
+    <Paper className="country-page" elevation={0}>
+      <CountryCard country={country}></CountryCard>
+    </Paper>
+  )
 }
