66a912e84b1a5b0ac77835a634dc9a05fc428824
Yahya Salimi <salimiyahya549@gmail.com>
2026-05-26 12:27:47 +0300

    reset everything

--- src/components/countryCard/FlagImage.tsx
+++
@@ -1,5 +0,0 @@
-import { Country } from '../../redux/types';
-
-export default function FlagImage({ country }: { country: Country }) {
-  return <img src={country.flag} alt={`Flag of ${country.name}`} className="flag" />;
-}
